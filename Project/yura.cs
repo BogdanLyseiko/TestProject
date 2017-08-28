2a38e68dbd5c5de4751fdc0c299311a272760e85
wds <wds@it-step.edu>
2017-08-28 20:45:30 +0300

    yura commit

---
+++ Project/yura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class CreditCard
+    {
+       public CreditCard(string n)
+        {
+            Name = n;
+        }
+        public string Name { get; set; }
+        public void WriteName()
+        {
+            if (Name != null)
+                Console.WriteLine(Name);
+        }
+    }
+}
